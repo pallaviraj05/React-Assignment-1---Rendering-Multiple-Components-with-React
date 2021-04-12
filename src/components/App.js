7a74155f999719983cc4a539f0e8922eab4962f9
pallaviraj05 <pallavi05031998@gmail.com>
2021-04-13 00:24:20 +0530

    myreactproject

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1>Myntra Clone</h1>
-      <p>It is a eCommerce platform and having ui experience</p>
+      <h1 className="data-ns-test" value="project-name">Myntra Clone</h1>
+      <p className="data-ns-test" value="project-description">It is a eCommerce platform and having ui experience</p>
     </div>
   )
 }
