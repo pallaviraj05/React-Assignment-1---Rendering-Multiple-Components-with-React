f2d37e71187f7faed06f54055d6e391d55793430
pallaviraj05 <pallavi05031998@gmail.com>
2021-04-13 00:03:19 +0530

    reactproject

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1>Myntra Clone</h1>
+      <p>It is a eCommerce platform and having ui experience</p>
+    </div>
   )
 }
 
