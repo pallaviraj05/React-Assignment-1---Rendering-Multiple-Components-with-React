eb1cec961dcc5d89b25ef999effa67e0c8f99f22
pallaviraj05 <pallavi05031998@gmail.com>
2021-04-17 22:46:54 +0530

    multiplecomponentredering

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 className="data-ns-test" value="project-name">Myntra Clone</h1>
-      <p className="data-ns-test" value="project-description">It is a eCommerce platform and having ui experience</p>
+      <h1 data-ns-test ="project-name">Myntra Clone</h1>
+      <p data-ns-test ="project-description">It is a eCommerce platform and having ui experience</p>
     </div>
   )
 }
